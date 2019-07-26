dbdffbe53a9778f6d345201a9388249f0cef5d8f
Keran Li <kerli@microsoft.com>
2019-07-26 16:57:29 +0800

    LD

---
+++ samples/csharp/dotnetcore/console/LD4/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarrenLee.Media;
+using NAudio.Wave;
+
+namespace LieDetector3
+{
+    public partial class Form1 : Form
+    {
+        //string ROOT = Application.StartupPath + "\\";
+        string ROOT = @"C:\\Hackthon\\";
+
+        int count = 0;
+        Camera myCamera = new Camera();
+        [System.Runtime.InteropServices.DllImport("winmm.dll")]
+        private static extern long mciSendString(string command, StringBuilder retstring, int ReturnLenth, IntPtr callback);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            mciSendString("Open new Type waveaudio alias recsound", null, 0, IntPtr.Zero);
+            button1.Click += new EventHandler(this.Button1_Click);
+            GetInfo();
+            myCamera.OnFrameArrived += MyCamera_OnFrameArrived;
+        }
+
+        private void GetInfo()
+        {
+            var cameraDevices = myCamera.GetCameraSources();
+            var cameraResolutions = myCamera.GetSupportedResolutions();
+            foreach (var d in cameraDevices)
+            {
+                cmb_CameraDevice.Items.Add(d);
+            }
+            foreach (var r in cameraResolutions)
+            {
+                cmb_CameraResolution.Items.Add(r);
+            }
+            cmb_CameraDevice.SelectedIndex = 0;
+            cmb_CameraResolution.SelectedIndex = 0;
+        }
+
+        private void MyCamera_OnFrameArrived(object source, FrameArrivedEventArgs e)
+        {
+            Image img = e.GetFrame();
+            picCamera.Image = img;
+        }
+
+        private void Cmb_CameraDevice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myCamera.ChangeCamera(cmb_CameraDevice.SelectedIndex);
+        }
+
+        private void Cmb_CameraResolution_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myCamera.Start(cmb_CameraResolution.SelectedIndex);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            myCamera.Stop();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            mciSendString("record recsound", null, 0, IntPtr.Zero);
+            button2.Click += new EventHandler(this.Button2_Click);
+            string filePath = Application.StartupPath + @"\" + "image_" + count.ToString();
+            ROOT=ROOT+"\\"+ DateTime.Now.ToString("yyyyMMddHHmmss")+"\\";
+            Directory.CreateDirectory(ROOT);
+            for (int i = 0; i < 5; i++)
+            {
+                filePath = ROOT + @"\image_" + i.ToString();
+                myCamera.Capture(filePath);
+                wait(3000);
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+            mciSendString("save recsound " + ROOT + @"record.wav", null, 0, IntPtr.Zero);
+            mciSendString("close recsound ", null, 0, IntPtr.Zero);
+            var input = File.ReadAllBytes(ROOT + @"record.wav");
+            var output = ConvertWavTo8000Hz16BitMonoWav(input);
+            File.WriteAllBytes(ROOT + "record_16.wav", output);
+            button3.Click += new EventHandler(this.Button3_Click);
+
+        }
+        private async void Button3_Click(object sender, EventArgs e)
+        {
+            var pair = await SpeechRecognization.ContinuousRecognitionWithFileAsync(ROOT);
+            //var pair = new KeyValuePair<string, double>("teststr", 10.0);
+            textBox1.Text = pair.Key;
+            double score = pair.Value;
+            LyingScoreBox.Text = Math.Round(score, 2) + "";
+
+            //MessageBox.Show(pair.Key+pair.Value);
+            if (score < 40)
+            {
+                LyingScoreBox.ForeColor = Color.Green;
+
+                //MessageBox.Show("No lies\nlying score: " + score);
+            }
+            else if (score < 75)
+            {
+                //MessageBox.Show("You may be lying!");
+                LyingScoreBox.ForeColor = Color.Yellow;
+            }
+            else
+            {
+                //MessageBox.Show("You are lying!!!");
+                LyingScoreBox.ForeColor = Color.Red;
+            }
+            //MessageBox.Show(pair.Key+":"+pair.Value);
+
+
+            
+        }
+        private async void Button6_Click(object sender, EventArgs e)
+        {
+            var pair_cv = await ImageUnderstanding.ImageUnderstandingAsync(ROOT);
+            //MessageBox.Show(pair_cv.Item1 + " " + pair_cv.Item2);
+            pictureBox1.Image = Image.FromFile(ROOT + @"\Image_" + pair_cv.Item1.ToString() + ".jpg");
+
+            double score2 = pair_cv.Item2;
+            LyingScoreBox2.Text = Math.Round(score2, 2) + "";
+            if (score2 < 40)
+            {
+                LyingScoreBox2.ForeColor = Color.Green;
+
+                //MessageBox.Show("No lies\nlying score: " + score);
+            }
+            else if (score2 < 75)
+            {
+                //MessageBox.Show("You may be lying!");
+                LyingScoreBox2.ForeColor = Color.Yellow;
+            }
+            else
+            {
+                //MessageBox.Show("You are lying!!!");
+                LyingScoreBox2.ForeColor = Color.Red;
+            }
+        }
+        public byte[] ConvertWavTo8000Hz16BitMonoWav(byte[] inArray)
+        {
+            using (var mem = new MemoryStream(inArray))
+            using (var reader = new WaveFileReader(mem))
+            using (var converter = WaveFormatConversionStream.CreatePcmStream(reader))
+            using (var upsampler = new WaveFormatConversionStream(new WaveFormat(8000, 16, 1), converter))
+            {
+                // todo: without saving to file using MemoryStream or similar
+                WaveFileWriter.CreateWaveFile(ROOT + "record_temp.wav", upsampler);
+                return File.ReadAllBytes(ROOT + "record_temp.wav");
+            }
+        }
+        public void wait(int milliseconds)
+        {
+            System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+            //Console.WriteLine("start wait timer");
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                //Console.WriteLine("stop wait timer");
+            };
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void PicCamera_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ samples/csharp/dotnetcore/console/LD4/ImageUnderstanding.cs
@@ -0,0 +1,225 @@
+﻿
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LieDetector3
+{
+    class ImageUnderstanding
+    {
+        private static Dictionary<string, int> lieDict = new Dictionary<string, int>()
+            {
+                {"smiling",200 },
+                {"sugar",1000 },
+                {"glasses",50 },
+                {"man",50 },
+                {"men",500 },
+                {"food",500 },
+                {"drink",500 },
+                {"drinks",500 },
+                {"room",50 },
+                {"car",500 },
+
+
+            };
+
+        // subscriptionKey = "0123456789abcdef0123456789ABCDEF"
+        private const string subscriptionKey = "a2a234d00c814018bb0a5d5191a11888";
+
+        // localImagePath = @"C:\Documents\LocalImage.jpg"
+        private const string localImagePath = @"D:\work and live in Microsoft\picture\2017-09\RQRP0740.jpg";
+
+        private const string remoteImageUrl =
+            "https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg";
+
+        // Specify the features to return
+        private static readonly List<VisualFeatureTypes> features =
+            new List<VisualFeatureTypes>()
+        {
+            VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+            VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+            VisualFeatureTypes.Tags,VisualFeatureTypes.Faces,
+            VisualFeatureTypes.Adult
+        };
+        static string ROOT = @"C:\\Hackthon\\";
+
+        static void test(string[] args)
+        {
+            ComputerVisionClient computerVision = new ComputerVisionClient(
+                new ApiKeyServiceClientCredentials(subscriptionKey),
+                new System.Net.Http.DelegatingHandler[] { });
+
+            // You must use the same region as you used to get your subscription
+            // keys. For example, if you got your subscription keys from westus,
+            // replace "westcentralus" with "westus".
+            //
+            // Free trial subscription keys are generated in the "westus"
+            // region. If you use a free trial subscription key, you shouldn't
+            // need to change the region.
+
+            // Specify the Azure region
+            computerVision.Endpoint = "https://eastasia.api.cognitive.microsoft.com";
+
+            Console.WriteLine("Images being analyzed ...");
+            var t1 = AnalyzeRemoteAsync(computerVision, remoteImageUrl);
+            var t2 = AnalyzeLocalAsync(computerVision, localImagePath);
+
+            Task.WhenAll(t1, t2).Wait(5000);
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+
+        public static async Task<Tuple<int,double>> ImageUnderstandingAsync(string imagePathfolder)
+        {
+            ComputerVisionClient computerVision = new ComputerVisionClient(
+                new ApiKeyServiceClientCredentials(subscriptionKey),
+                new System.Net.Http.DelegatingHandler[] { });
+            computerVision.Endpoint = "https://eastasia.api.cognitive.microsoft.com";
+            Dictionary<string, int> tagFreq = new Dictionary<string, int>();
+            int index = -1;
+            double score = -99;
+            for(int i = 0; i < 5; i++)
+            {
+                string imagePath = imagePathfolder + "\\image_" + i + ".jpg";
+                
+                if (!File.Exists(imagePath))
+                {
+                    Console.WriteLine(
+                        "\nUnable to open or read localImagePath:\n{0} \n", imagePath);
+                    return null;
+                }
+            
+                using (Stream imageStream = File.OpenRead(imagePath))
+                {
+
+                    ImageAnalysis analysis = await computerVision.AnalyzeImageInStreamAsync(
+                        imageStream, features);
+                    if (analysis.Description.Captions.Count != 0)
+                    {
+                        HashSet<string> tagsSet = new HashSet<string>();
+                        foreach (var tag in analysis.Description.Tags)
+                        {
+                            tagsSet.Add(tag);
+                            if (!tagFreq.ContainsKey(tag))
+                                tagFreq.Add(tag, 1);
+                            else
+                                tagFreq[tag] = tagFreq[tag] + 1;
+                        }
+                        double s = isLyingImage(tagsSet);
+                        if (s > score)
+                        {
+                            score = s;
+                            index = i;
+                        }
+                        //Console.WriteLine(analysis.Description.Captions[0].Text + "\n");
+                        //Console.WriteLine(string.Join(",", analysis.Description.Tags) + "\n");
+                        //if (analysis.Adult != null)
+                        //    Console.WriteLine(analysis.Adult.IsAdultContent + "\n");
+                        //Console.WriteLine(analysis.Adult.IsRacyContent + "\n");
+                        //if (analysis.Faces.Count > 0)
+                        //    Console.WriteLine(analysis.Faces[0].Age + "\n");
+                        //Console.WriteLine(analysis.Faces[0].Gender.ToString() + "\n");
+
+                    }
+                    //else
+                    //{
+                    //    Console.WriteLine("No description generated.");
+                    //}
+                }
+            }
+            double lying_score = isLyingImage(tagFreq);
+            return new Tuple<int, double>(index, lying_score);
+            
+        }
+
+        private static double isLyingImage(Dictionary<string, int> tagFreq)
+        {
+            int logit = -100;
+            
+            foreach (KeyValuePair<string, int> pair in lieDict)
+            {
+                if (tagFreq.ContainsKey(pair.Key))
+                {
+                    logit += pair.Value;
+                }
+            }
+
+            return 100.0 / (1.0 + Math.Exp(-logit / 10));
+        }
+        private static double isLyingImage(HashSet<string> tagFreq)
+        {
+            int logit = -100;
+
+            foreach (KeyValuePair<string, int> pair in lieDict)
+            {
+                if (tagFreq.Contains(pair.Key))
+                {
+                    logit += pair.Value;
+                }
+            }
+
+            return 100.0 / (1.0 + Math.Exp(-logit / 10));
+        }
+        // Analyze a remote image
+
+        private static async Task AnalyzeRemoteAsync(
+            ComputerVisionClient computerVision, string imageUrl)
+        {
+            if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
+            {
+                Console.WriteLine(
+                    "\nInvalid remoteImageUrl:\n{0} \n", imageUrl);
+                return;
+            }
+
+            ImageAnalysis analysis =
+                await computerVision.AnalyzeImageAsync(imageUrl, features);
+            DisplayResults(analysis, imageUrl);
+        }
+
+        // Analyze a local image
+        private static async Task AnalyzeLocalAsync(
+            ComputerVisionClient computerVision, string imagePath)
+        {
+            if (!File.Exists(imagePath))
+            {
+                Console.WriteLine(
+                    "\nUnable to open or read localImagePath:\n{0} \n", imagePath);
+                return;
+            }
+
+            using (Stream imageStream = File.OpenRead(imagePath))
+            {
+                ImageAnalysis analysis = await computerVision.AnalyzeImageInStreamAsync(
+                    imageStream, features);
+                DisplayResults(analysis, imagePath);
+            }
+        }
+
+        // Display the most relevant caption for the image
+        private static void DisplayResults(ImageAnalysis analysis, string imageUri)
+        {
+            Console.WriteLine(imageUri);
+            if (analysis.Description.Captions.Count != 0)
+            {
+                Console.WriteLine(analysis.Description.Captions[0].Text + "\n");
+                Console.WriteLine(string.Join(",", analysis.Description.Tags) + "\n");
+                if (analysis.Adult != null)
+                    Console.WriteLine(analysis.Adult.IsAdultContent + "\n");
+                Console.WriteLine(analysis.Adult.IsRacyContent + "\n");
+                if (analysis.Faces.Count > 0)
+                    Console.WriteLine(analysis.Faces[0].Age + "\n");
+                Console.WriteLine(analysis.Faces[0].Gender.ToString() + "\n");
+
+            }
+            else
+            {
+                Console.WriteLine("No description generated.");
+            }
+        }
+    }
+}
